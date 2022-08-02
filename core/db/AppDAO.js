d253934069155e0ba639141f2cf9eda4e2cb41e0
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2022-08-01 20:25:27 -0500

    version 0.2

--- core/db/AppDAO.js
+++ core/db/AppDAO.js
@@ -1,7 +1,5 @@
 const sqlite3 = require('sqlite3')
 
-// const Promise = require('bluebird')
-
 class AppDAO {
     constructor(dbFilePath) {
         this.db = new sqlite3.Database(dbFilePath, (err) => {
