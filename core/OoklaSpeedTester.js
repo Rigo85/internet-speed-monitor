be6cc9ff82b8af28766338dfe4fed036d9a2c0e1
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2022-08-06 19:04:21 -0500

    fixing speedtest for win32

--- core/OoklaSpeedTester.js
+++ core/OoklaSpeedTester.js
@@ -2,10 +2,25 @@
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const path = require("path");
 
 async function speedTest() {
+    let ext;
+    switch (process.platform) {
+        case 'darwin':
+            ext = '';
+            break;
+        case 'win32':
+            ext = '.exe';
+            break;
+        default:
+            ext = '';
+            break;
+    }
+
     try {
-        const {stdout, stderr} = await exec(`core/ookla-speedtest/${process.platform}/speedtest -f json`);
+        const speedTestPath = path.join("core","ookla-speedtest",process.platform,`speedtest${ext}`);
+        const {stdout, stderr} = await exec(`${speedTestPath} -f json`);
         if (stderr) {
             console.info("speedTest", stderr);
             return undefined;
